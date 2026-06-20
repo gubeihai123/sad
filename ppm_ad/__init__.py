41734e92482df96699a25c36bf96f656498d7b9e
BoYuan Wang <2700035833@qq.com>
2026-06-20 23:04:13 +0800

    Add ppm_ad/__init__.py

---
+++ ppm_ad/__init__.py
@@ -0,0 +1,8 @@
+"""Plastic Prototype Memory for industrial anomaly detection."""
+
+from .models import PatchMemory, PlasticPrototypeMemory
+
+__all__ = ["PatchMemory", "PlasticPrototypeMemory"]
+__version__ = "0.1.0"
+
+
